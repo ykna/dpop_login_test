1c71b64ce4dfcda2623887f0214a91cb4e70d378
那云开 <2790522457@qq.com>
2019-01-22 16:25:16 +0800

    add test.spec.02.js

---
+++ jscript/test.spec.02.js
@@ -0,0 +1,277 @@
+const fs = require('fs');
+const path = require('path');
+const chai = require("chai");
+const should = chai.should();
+const JWebDriver = require('jwebdriver');
+chai.use(JWebDriver.chaiSupportChainPromise);
+const resemble = require('resemblejs-node');
+resemble.outputSettings({
+    errorType: 'flatDifferenceIntensity'
+});
+
+const rootPath = getRootPath();
+
+module.exports = function(){
+
+    let driver, testVars;
+
+    before(function(){
+        let self = this;
+        driver = self.driver;
+        testVars = self.testVars;
+    });
+
+    it('url: http://www.baidu.com', async function(){
+        await driver.url(_(`http://www.baidu.com`));
+    });
+
+    it('waitBody: ', async function(){
+        await driver.sleep(500).wait('body', 30000).html().then(function(code){
+            isPageError(code).should.be.false;
+        });
+    });
+
+    it('click: wd ( #kw, 138, 11, 0 )', async function(){
+        await driver.sleep(300).wait('#kw', 30000)
+               .sleep(300).mouseMove(138, 11).click(0);
+    });
+
+    it('click: wd ( #kw, 120, 12, 0 )', async function(){
+        await driver.sleep(300).wait('#kw', 30000)
+               .sleep(300).mouseMove(120, 12).click(0);
+    });
+
+    it('sendKeys: hello', async function(){
+        await driver.sendKeys('hello');
+    });
+
+    it('dblClick: wd ( #kw, 93, 12, 0 )', async function(){
+        await driver.sleep(300).wait('#kw', 30000)
+               .sleep(300).mouseMove(93, 12).click(0).click(0);
+    });
+
+    it('click: wd ( #kw, 93, 12, 0 )', async function(){
+        await driver.sleep(300).wait('#kw', 30000)
+               .sleep(300).mouseMove(93, 12).click(0);
+    });
+
+    it('insertVar: wd ( #kw, {{username}} )', async function(){
+        await driver.sleep(300).wait('#kw', 30000)
+               .val(_(`{{username}}`));
+    });
+
+    it('mouseMove: java学习路线 ( #form li:nth-child(1), 231, 8 )', async function(){
+        await driver.sleep(300).wait('#form li:nth-child(1)', 30000)
+               .sleep(300).mouseMove(231, 8);
+    });
+
+    it('click: java学习路线 ( #form li.bdsug-s, 275, 4, 0 )', async function(){
+        await driver.sleep(300).wait('#form li.bdsug-s', 30000)
+               .sleep(300).mouseMove(275, 4).click(0);
+    });
+
+    it('click: 百度一下 ( #su, 32, 12, 0 )', async function(){
+        await driver.sleep(300).wait('#su', 30000)
+               .sleep(300).mouseMove(32, 12).click(0);
+    });
+
+    it('× click: java 学习路线图 实战... ( #content_left div[id="3001"] > div.drkSJw > h3.YNbECw > a.umdLxv, 175, 11, 0 )', async function(){
+        await driver.sleep(300).wait('#content_left div[id="3001"] > div.drkSJw > h3.YNbECw > a.umdLxv', 30000)
+               .sleep(300).mouseMove(175, 11).click(0);
+    });
+
+    it('× switchWindow: 1', async function(){
+        await driver.sleep(500).switchWindow(1);
+    });
+
+    it('waitBody: ', async function(){
+        await driver.sleep(500).wait('body', 30000).html().then(function(code){
+            isPageError(code).should.be.false;
+        });
+    });
+
+    function _(str){
+        if(typeof str === 'string'){
+            return str.replace(/\{\{(.+?)\}\}/g, function(all, key){
+                return testVars[key] || '';
+            });
+        }
+        else{
+            return str;
+        }
+    }
+
+};
+
+if(module.parent && /mocha\.js/.test(module.parent.id)){
+    runThisSpec();
+}
+
+function runThisSpec(){
+    // read config
+    let webdriver = process.env['webdriver'] || '';
+    let proxy = process.env['wdproxy'] || '';
+    let config = require(rootPath + '/config.json');
+    let webdriverConfig = Object.assign({},config.webdriver);
+    let host = webdriverConfig.host;
+    let port = webdriverConfig.port || 4444;
+    let match = webdriver.match(/([^\:]+)(?:\:(\d+))?/);
+    if(match){
+        host = match[1] || host;
+        port = match[2] || port;
+    }
+    let testVars = config.vars;
+    let browsers = webdriverConfig.browsers;
+    browsers = browsers.replace(/^\s+|\s+$/g, '');
+    delete webdriverConfig.host;
+    delete webdriverConfig.port;
+    delete webdriverConfig.browsers;
+
+    // read hosts
+    let hostsPath = rootPath + '/hosts';
+    let hosts = '';
+    if(fs.existsSync(hostsPath)){
+        hosts = fs.readFileSync(hostsPath).toString();
+    }
+    let specName = path.relative(rootPath, __filename).replace(/\\/g,'/').replace(/\.js$/,'');
+
+    browsers.split(/\s*,\s*/).forEach(function(browserName){
+        let caseName = specName + ' : ' + browserName;
+
+        let browserInfo = browserName.split(' ');
+        browserName = browserInfo[0];
+        let browserVersion = browserInfo[1];
+
+        describe(caseName, function(){
+
+            this.timeout(600000);
+            this.slow(1000);
+
+            let driver;
+            before(function(){
+                let self = this;
+                let driver = new JWebDriver({
+                    'host': host,
+                    'port': port
+                });
+                let sessionConfig = Object.assign({}, webdriverConfig, {
+                    'browserName': browserName,
+                    'version': browserVersion,
+                    'ie.ensureCleanSession': true,
+                    'chromeOptions': {
+                        'args': ['--enable-automation']
+                    }
+                });
+                if(proxy){
+                    sessionConfig.proxy = {
+                        'proxyType': 'manual',
+                        'httpProxy': proxy,
+                        'sslProxy': proxy
+                    }
+                }
+                else if(hosts){
+                    sessionConfig.hosts = hosts;
+                }
+
+                try {
+                    self.driver = driver.session(sessionConfig).windowSize(1024, 768).config({
+                        pageloadTimeout: 30000, // page onload timeout
+                        scriptTimeout: 5000, // sync script timeout
+                        asyncScriptTimeout: 10000 // async script timeout
+                    });
+                } catch (e) {
+                    console.log(e);
+                }
+
+                self.testVars = testVars;
+                let casePath = path.dirname(caseName);
+                self.screenshotPath = rootPath + '/screenshots/' + casePath;
+                self.diffbasePath = rootPath + '/diffbase/' + casePath;
+                self.caseName = caseName.replace(/.*\//g, '').replace(/\s*[:\.\:\-\s]\s*/g, '_');
+                mkdirs(self.screenshotPath);
+                mkdirs(self.diffbasePath);
+                self.stepId = 0;
+                return self.driver;
+            });
+
+            module.exports();
+
+            beforeEach(function(){
+                let self = this;
+                self.stepId ++;
+                if(self.skipAll){
+                    self.skip();
+                }
+            });
+
+            afterEach(async function(){
+                let self = this;
+                let currentTest = self.currentTest;
+                let title = currentTest.title;
+                if(currentTest.state === 'failed' && /^(url|waitBody|switchWindow|switchFrame):/.test(title)){
+                    self.skipAll = true;
+                }
+                if(!/^(closeWindow):/.test(title)){
+                    let filepath = self.screenshotPath + '/' + self.caseName + '_' + self.stepId;
+                    let driver = self.driver;
+                    try{
+                        // catch error when get alert msg
+                        await driver.getScreenshot(filepath + '.png');
+                        let url = await driver.url();
+                        let html = await driver.source();
+                        html = '<!--url: '+url+' -->\n' + html;
+                        fs.writeFileSync(filepath + '.html', html);
+                        let cookies = await driver.cookies();
+                        fs.writeFileSync(filepath + '.cookie', JSON.stringify(cookies));
+                    }
+                    catch(e){}
+                }
+            });
+
+            after(function(){
+                return this.driver.close();
+            });
+
+        });
+    });
+}
+
+function getRootPath(){
+    let rootPath = path.resolve(__dirname);
+    while(rootPath){
+        if(fs.existsSync(rootPath + '/config.json')){
+            break;
+        }
+        rootPath = rootPath.substring(0, rootPath.lastIndexOf(path.sep));
+    }
+    return rootPath;
+}
+
+function mkdirs(dirname){
+    if(fs.existsSync(dirname)){
+        return true;
+    }else{
+        if(mkdirs(path.dirname(dirname))){
+            fs.mkdirSync(dirname);
+            return true;
+        }
+    }
+}
+
+function callSpec(name){
+    try{
+        require(rootPath + '/' + name)();
+    }
+    catch(e){
+        console.log(e)
+        process.exit(1);
+    }
+}
+
+function isPageError(code){
+    return code == '' || / jscontent="errorCode" jstcache="\d+"|diagnoseConnectionAndRefresh|dnserror_unavailable_header|id="reportCertificateErrorRetry"|400 Bad Request|403 Forbidden|404 Not Found|500 Internal Server Error|502 Bad Gateway|503 Service Temporarily Unavailable|504 Gateway Time-out/i.test(code);
+}
+
+function catchError(error){
+
+}
